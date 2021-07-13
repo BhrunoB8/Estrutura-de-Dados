c54caa5ee64a5783da764ba4335eabedf8acdae7
BhrunoB8 <bhruno.roan@yahoo.com>
2021-07-13 15:58:03 -0300

    Update fibonacci.java

--- Marco 1/URI exercícios/URI/fibonacci.java
+++ Marco 1/URI exercícios/URI/fibonacci.java
@@ -11,18 +11,41 @@ import java.util.Scanner;
  *
  * @author Usuario
  */
+
+import java.util.Scanner;
+
 public class fibonacci {
- 
-    public static void main(String[] args) throws IOException {
-    Scanner entrada = new Scanner(System.in);
-    
-    int T, N;
-    int i =0;
-    
-    T= entrada.nextInt();
-         while(i < T){
-              N = entrada.nextInt();
-              if(N >= 0 && N<=60){
+
+    public static void main(String[] args) {
+        int T, N;
+        int j = 0;
+
+
+        int fibo[] = new int[60];
+         Scanner sc = new Scanner(System.in);
+         T = sc.nextInt();
+         while (j < T) {
+
+         N = sc.nextInt();
+         if (N >= 0 && N <= 60)
+         for (int i = 0; i < fibo.length; i++) {
+         if (i == 0) {
+         fibo[i] = 0;
+         } else if (i == 1) {
+         fibo[i] = 1;
+         } else {
+         fibo[i] = fibo[i - 1] + fibo[i - 2];
+         }
+         if (N == i) {
+         System.out.println("Fib(" + N + ") = " + fibo[i]);
+         }
+
+         }
+         j++;
+         }
+    }
+
+}
               
                   
                
