a92d250a83d1f61c1386292816ae52c4dff8ade0
Rafael <31259711+rafaelbtorres@users.noreply.github.com>
2021-09-27 21:26:46 -0300

    adicionado nova feature

--- APG/APG/src/DuplamenteEncadeada.java
+++ APG/APG/src/DuplamenteEncadeada.java
@@ -1,300 +1,186 @@
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
 
-// import java.util.Scanner;
+public class DuplamenteEncadeada<T> {
 
-public class DuplamenteEncadeada {
+    private No<T> head;
+    private No<T> tail;
+    private ArrayList <T> Nos = new ArrayList();
+    private Integer size = 0;
 
-    private No head;
-    private No tail;
-    private int tamanho;
-    private int quantidade;
-    private No[] nos;
+    public void adicionarInicio(T dado) {
+        No<T> novoNo = new No<>(dado);
 
-    public boolean isVazio() {
-        if (head == null && tail == null)
-            return true;
-        else
-            return false;
+        novoNo.proximo = head;
+        novoNo.anterior = null;
+
+        if (head != null)
+            head.anterior = novoNo;
+
+        head = novoNo;
+        
+        if (tail == null)
+            tail = novoNo;
+        size++;
     }
 
-    public void adicionarhead(No novo) {
+    public void adicionarFim(T dado) {
+        No<T> novoNo = new No<>(dado);
+        No<T> ultimo = head;
+        Nos.add(dado);
+        System.out.println(dado.toString());
+        novoNo.proximo = null;
+        if (tail != null)
+            tail.proximo = novoNo;
+        tail = novoNo;
 
-        if (isVazio()) {
-            head = novo;
-            tail = novo;
-        } else {
-            novo.proximo = head;
-            head = novo;
-            novo.proximo.anterior = head;
-        }
-    }
-
-    public void adicionarFinal(No novo) {
-
-        if (isVazio()) {
-            head = novo;
-            tail = novo;
-        } else {
-            novo.anterior = tail;
-            tail = novo;
-            novo.anterior.proximo = tail;
-        }
-    }
-
-    public void imprimir() {
-        No aux = head;
-
-        while (aux != null) {
-            System.out.println("No: " + aux.getEstudante().toString());
-            if (aux.anterior != null)
-                System.out.println("Anterior: " + aux.anterior.getEstudante().toString());
-            if (aux.proximo != null)
-                System.out.println("Posterior: " + aux.proximo.getEstudante().toString());
-            System.out.println("------------------------------------");
-            aux = aux.proximo;
-        }
-
-    }
-
-    public No inserirhead(Estudante info) {
-        No no = new No();
-        no.estudante = info;
-        no.anterior = null;
-        no.proximo = head;
-
-        // **INSERÇÕES */
-        // Insere no head da Lista
-        if (head != null) {
-            head.anterior = no;
-
-        }
-        head = no;
-        if (tamanho == 0) {
-            tail = head;
-        }
-        tamanho++;
-        if (tamanho == 1) {
-            return no;
-        }
-        return null;
-    }
-
-    // public void inserirInicio(No no) {
-    // if (isVazio()) {
-    // head = no;
-    // tail = no;
-    // } else {
-    // no.setProximo(this.head);
-    // head.setProximo(no);
-    // this.head = no;
-    // }
-    // quantidade++;
-    // }
-
-    // public void inserir(No no, int posicao) {
-    // if (isVazio() || posicao <= 1) {
-    // inserirInicio(no);
-    // // } else if (posicao > quantidade) {
-    // // inserirhead(no)
-    // } else {
-    // No aux = this.head;
-    // for (int i = 1; i < posicao; i++) {
-    // aux = aux.getProximo();
-    // }
-    // no.setProximo(aux);
-    // no.setAnterior(aux.getAnterior());
-    // no.getAnterior().setProximo(no);
-    // aux.setAnterior(no);
-    // }
-    // this.quantidade++;
-    // }
-
-    public No[] inserirEstudantes() {
-
-        int q = 10;
-        nos = new No[q];
-        for (int i = 0; i < q; i++) {
-            No n = new No();
-            Estudante es = new Estudante();
-            n.setEstudante(es);
-            nos[i] = n;
-            inserirhead(es);
-            System.out.println(n.estudante.toString());
-            quantidade++;
-            // if(i==quantidade-1){
-            // System.out.println(nos[i].estudante.toString());
-            // return nos;
-            // }else{
-            // System.out.println("");
-            // }
-            //
-        }
-        return nos;
-    }
-
-    public Estudante retirarInicio() {
         if (head == null) {
-            return null;
-        }
-        Estudante out = head.estudante;
-        head = head.proximo;
-        if (head != null) {
-            head.anterior = null;
-        } else {
-            tail = null;
-        }
-        tamanho--;
-        return out;
-    }
-
-    public No removerEstudante() {
-        int count = 1;
-        No aux = nos[count-1];
-        if (!isVazio() && count >= 1 && count <= quantidade) {
-            count++;
-            if (count == 1) {
-                head = aux.getProximo();
-                if (head != null) {
-
-                    head.setAnterior(null);
-                } else if (count == quantidade) {
-
-                    aux = tail;
-                    tail = tail.getAnterior();
-                    tail.setProximo(null);
-                }
-            } else {
-                for (int i = 1; i < count; i++) {
-                    aux = aux.getProximo();
-                }
-                aux.getAnterior().setProximo(aux.getProximo());
-                aux.getProximo().setAnterior(aux.getAnterior());
-            }
-            if (aux.estudante.getMatricula() < 202060000) {
-
-                aux.setAnterior(null);
-                aux.setProximo(null);
-                quantidade--;
-            }
+            head = novoNo;
         } else {
 
+            while (ultimo.proximo != null) {
+                ultimo = ultimo.proximo;
+            }
+            ultimo.proximo = novoNo;
+            novoNo.anterior = ultimo;
         }
-        return aux;
+        size++;
     }
 
-    public No remover(int posicao) {
-        No aux = head;
-        if (!isVazio() && posicao >= 1 && posicao <= quantidade) {
-            if (posicao == 1) {
-                head = aux.getProximo();
-                if (head != null) {
+    public void remove(No<T> no) {
 
-                    head.setAnterior(null);
-                } else if (posicao == quantidade) {
+        System.out.println("Estudante removido com sucesso!");
+        if (head == null || no == null)
+            return;
 
-                    aux = tail;
-                    tail = tail.getAnterior();
-                    tail.setProximo(null);
-                }
-            } else {
-                for (int i = 1; i < posicao; i++) {
-                    aux = aux.getProximo();
-                }
-                aux.getAnterior().setProximo(aux.getProximo());
-                aux.getProximo().setAnterior(aux.getAnterior());
+        if (head == no)
+            head = no.proximo;
+        
+        if (no.proximo != null)
+            no.proximo.anterior = no.anterior;
+
+        if (no.anterior != null)
+            no.anterior.proximo = no.proximo;
+
+        size--;
+    }
+
+    public No<T> getPrimeiro() {
+        return head;
+    }
+
+    public No<T> get(Integer index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Passou do limite!");
+        } else {
+            No<T> aux = head;
+            for (int i = 0; i < index; i++) {
+                aux = aux.proximo;
             }
-            aux.setProximo(null);
-            aux.setAnterior(null);
-            quantidade--;
+            return aux;
         }
-        return aux;
+    }
+
+    public Integer size() {
+        return this.size;
+    }
+
+
+    public static void pesquisar(DuplamenteEncadeada<Map<Integer, String>> lista, String curso) {
+        int qtd = 0;
+        boolean duplicada;
+
+        for (int i = 0; i < lista.size(); i++) {
+            Map<Integer, String> entry = lista.get(i).getDado();
+
+            for (Integer key : entry.keySet()) {
+                String value = entry.get(key);
+
+                if (value.equals(curso)) {
+                    duplicada = false;
+
+                    for (int j = i + 1; j < lista.size(); j++) {
+                        Map<Integer, String> a = lista.get(j).getDado();
+                        if (entry.entrySet().equals(a.entrySet())) {
+                            duplicada = true;
+                        }
+
+                    }
+
+                    if (!duplicada)
+                        qtd++;
+
+                }
+
+            }
+
+        }
     }
 
     public static void main(String[] args) {
-        DuplamenteEncadeada l = new DuplamenteEncadeada();
-        // No n = new No();
+        Preenchimento prencher = new Preenchimento();
+        String curso = "Engenharia de Software";
+        Integer matricula = 202050000;
+ 
+        DuplamenteEncadeada<Map<Integer, String>> listaDuplamenteEncadeada = new DuplamenteEncadeada<>();
+
+        Scanner in = new Scanner(System.in);
+
+        int op;
         double startTime = System.nanoTime();
-        l.inserirEstudantes();
         double endTime = System.nanoTime();
         double timeElapsed = endTime - startTime;
-        System.out.print("Tempo de criação do vetor: " + timeElapsed / 1000000 + "ms");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        System.out.println("--------------------");
-        l.removerEstudante();
-        // for(int i=0; i<10; i++){
-        // Estudante e = new Estudante();
-        // n.setEstudante(e);
-        // l.adicionarhead(n);
-        // }
-        // l.imprimir();
-        // Estudante[] array = new Estudante[100000];
+        do {
+            System.out.println("1. Cadastrar todos estudantes");
+            System.out.println("2. Estudantes realizando o curso de ES");
+            System.out.println("3. Remover estudantes com matricula igual ou inferior a 202060000");
+            System.out.println("4. Sair");
+            op = in.nextInt();
 
-        // Scanner in = new Scanner(System.in);
+            switch (op) {
+                case 1:
+                    startTime = System.nanoTime();
+                    // doSelectionSort(Nos);
+                    listaDuplamenteEncadeada = prencher.getLDE();
+                    endTime = System.nanoTime();
+                    System.out.println(listaDuplamenteEncadeada.size());
+                    timeElapsed = endTime - startTime;
 
-        // int op;
-        // double startTime = System.nanoTime();
-        // v.inserirEstudante(array);
-        // double endTime = System.nanoTime();
-        // double timeElapsed = endTime - startTime;
-        // System.out.print("Tempo de criação do vetor: " + timeElapsed / 1000000 + "ms
-        // \n");
-        // do {
-        // System.out.println("1. Apresentar todos estudantes em ordem crescente de
-        // matrícula");
-        // System.out.println("2. Estudantes realizando o curso de ES");
-        // System.out.println("3. Remover estudantes com matricula igual ou inferior a
-        // 202060000");
-        // System.out.println("4. Sair");
-        // op = in.nextInt();
+                    System.out.println("==============================================");
+                    System.out.println("Execution time in nanoseconds: " + timeElapsed);
+                    System.out.println("Execution time in miliseconds: " + timeElapsed / 1000000);
+                    System.out.println("==============================================");
+                    break;
 
-        // switch (op) {
-        // case 1:
-        // startTime = System.nanoTime();
-        // mergeSort(array.length, array);
-        // endTime = System.nanoTime();
-        // timeElapsed = endTime - startTime;
+                case 2:
+                    startTime = System.nanoTime();
+                    Validar.pesquisar(listaDuplamenteEncadeada, curso);
+                    endTime = System.nanoTime();
+                    timeElapsed = endTime - startTime;
+                    System.out.println("==============================================");
+                    System.out.println("Execution time in nanoseconds: " + timeElapsed);
+                    System.out.println("Execution time in miliseconds: " + timeElapsed / 1000000);
+                    System.out.println("==============================================");
+                    break;
 
-        // imprimir();
-        // System.out.println("==============================================");
-        // System.out.println("Execution time in nanoseconds: " + timeElapsed);
-        // System.out.println("Execution time in miliseconds: " + timeElapsed /
-        // 1000000);
-        // System.out.println("==============================================");
-        // break;
+                case 3:
+                    startTime = System.nanoTime();
+                    Validar.remover(listaDuplamenteEncadeada, matricula);
+                    endTime = System.nanoTime();
+                    timeElapsed = endTime - startTime;
+                    System.out.println("==============================================");
+                    System.out.println("Execution time in nanoseconds: " + timeElapsed);
+                    System.out.println("Execution time in miliseconds: " + timeElapsed / 1000000);
+                    System.out.println("==============================================");
+                    break;// dianho no break
+                case 4:
+                    System.out.println("SAINDO...");
+                    in.close();
+            }
 
-        // case 2:
-        // startTime = System.nanoTime();
-
-        // System.out.println("Número de alunos que cursam ES: " +
-        // v.verificaCurso(array));
-        // endTime = System.nanoTime();
-        // timeElapsed = endTime - startTime;
-        // System.out.println("==============================================");
-        // System.out.println("Execution time in nanoseconds: " + timeElapsed);
-        // System.out.println("Execution time in miliseconds: " + timeElapsed /
-        // 1000000);
-        // System.out.println("==============================================");
-        // break;
-
-        // case 3:
-        // startTime = System.nanoTime();
-        // v.removeEstudantes(array);
-        // endTime = System.nanoTime();
-        // printaVetor(array);
-        // timeElapsed = endTime - startTime;
-        // System.out.println("==============================================");
-        // System.out.println("Execution time in nanoseconds: " + timeElapsed);
-        // System.out.println("Execution time in miliseconds: " + timeElapsed /
-        // 1000000);
-        // System.out.println("==============================================");
-        // break;// dianho no break
-        // case 4:
-        // System.out.println("SAINDO...");
-        // in.close();
-        // }
-
-        // System.out.println(timeElapsed);
-        // } while (op != 4);
+            System.out.println("Tempo de execução do sistema: "+timeElapsed);
+        } while (op != 4);
 
     }
 
--- APG/APG/src/No.java
+++ APG/APG/src/No.java
@@ -1,69 +1,35 @@
-//Classe No para a Lista DuplamenteEncadeada
-public class No {
+public class No<T> {
+    T dado;
+    No<T> proximo;
+    No<T> anterior;
 
-    public int elemento;
-    public No anterior;
-    public No proximo;
-    public Estudante estudante;
+   No(T d) {
+       dado = d;
+       proximo = null;
+       anterior = null;
+   }
 
-    public No(int elemento) {
-        this.elemento = elemento;
-        anterior = proximo = null;
-    }
+   public T getDado() {
+       return dado;
+   }
 
-    public No() {
+   public void setDado(T dado) {
+       this.dado = dado;
+   }
 
-    }
+   public No<T> getProximo() {
+       return proximo;
+   }
 
-    public Estudante getEstudante() {
-        return estudante;
-    }
+   public void setProximo(No<T> proximo) {
+       this.proximo = proximo;
+   }
 
-    public void setEstudante(Estudante estudante) {
-        this.estudante = estudante;
-    }
+   public No<T> getAnterior() {
+       return anterior;
+   }
 
-    public No getAnterior() {
-        return anterior;
-    }
-
-    public void setAnterior(No anterior) {
-        this.anterior = anterior;
-    }
-
-    public No getProximo() {
-        return proximo;
-    }
-
-    public void setProximo(No proximo) {
-        this.proximo = proximo;
-    }
-
-    public int getElemento(){
-        return elemento;
-    }
-
-    public void setElemento(int elemento) {
-        this.elemento = elemento;
-    }
-
-    // public void removerEstudantes() {
-    //     Estudante es = head.estudante;
-    //     No aux = head;
-    //     int count = 0;
-    //     if (!isVazio()) {
-    //         for (aux = head; aux == tail; aux = aux.proximo) {
-    //             count++;
-    //             if (es.getMatricula() < 202060000) {
-    //                 System.out.println("ok1");
-    //                 remover(count);
-    //             } else {
-    //                 System.out.println("ok2");
-    //                 System.out.println(es.toString());
-    //             }
-    //         }
-    //     } else {
-    //         System.out.println("ok3");
-    //     }
-    // }
+   public void setAnterior(No<T> anterior) {
+       this.anterior = anterior;
+   }
 }
